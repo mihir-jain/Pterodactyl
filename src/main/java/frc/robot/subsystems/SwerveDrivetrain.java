c0805932f3fd18e74d04a0354ce0b6786c973c7f
mihir-jain <mihirrjain200@gmail.com>
2021-01-22 17:51:27 -0800

    Working on finishing the subsytems and getting it to be buildable!!!

--- src/main/java/frc/robot/subsystems/SwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/SwerveDrivetrain.java
@@ -9,6 +9,7 @@ package frc.robot.subsystems;
 
 import com.kauailabs.navx.frc.AHRS;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj.geometry.Rotation2d;
@@ -17,6 +18,7 @@ import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.wpilibj.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 import frc.robot.common.AbsoluteEncoder;
 
 public class SwerveDrivetrain extends SubsystemBase {
@@ -24,18 +26,23 @@ public class SwerveDrivetrain extends SubsystemBase {
    * Creates a new ExampleSubsystem.
    */
   PIDController pidControl;
-  private CANSparkMax frontLeftDrive;
-  private CANSparkMax frontLeftTurn;
-  private CANSparkMax frontRightDrive;
-  private CANSparkMax frontRightTurn;
-  private CANSparkMax backLeftDrive;
-  private CANSparkMax backLeftTurn;
-  private CANSparkMax backRightDrive;
-  private CANSparkMax backRightTurn;
-  private AbsoluteEncoder frontLeftEncoder;
-  private AbsoluteEncoder frontRightEncoder;
-  private AbsoluteEncoder backLeftEncoder;
-  private AbsoluteEncoder backRightEncoder;
+  private SwerveWheel m_frontLeft;
+  private SwerveWheel m_backRight;
+  private SwerveWheel m_backLeft;
+  private SwerveWheel m_frontRight;
+
+  private CANSparkMax m_frontLeftDrive;
+  private CANSparkMax m_frontLeftTurn;
+  private CANSparkMax m_frontRightDrive;
+  private CANSparkMax m_frontRightTurn;
+  private CANSparkMax m_backLeftDrive;
+  private CANSparkMax m_backLeftTurn;
+  private CANSparkMax m_backRightDrive;
+  private CANSparkMax m_backRightTurn;
+  private AbsoluteEncoder m_frontLeftEncoder;
+  private AbsoluteEncoder m_frontRightEncoder;
+  private AbsoluteEncoder m_backLeftEncoder;
+  private AbsoluteEncoder m_backRightEncoder;
   private SwerveDriveKinematics kinematics;
   private Gyro m_gyro;
 
@@ -50,6 +57,35 @@ public class SwerveDrivetrain extends SubsystemBase {
     pidControl = new PIDController(1, 0, 0);
     pidControl.enableContinuousInput(0, 360);
     pidControl.setTolerance(10);
+
+    double widthOffset = Constants.Physical.widthInMeters / 2;
+    double lengthOffset = Constants.Physical.lengthInMeters / 2;
+
+    m_frontLeftDrive = new CANSparkMax(Constants.Drivetrain.frontLeftDriveMotorPort, MotorType.kBrushless);
+    m_frontLeftDrive.setInverted(true);
+    m_frontLeftTurn = new CANSparkMax(Constants.Drivetrain.frontLeftTurnMotorPort, MotorType.kBrushless);
+    m_frontLeftEncoder = new AbsoluteEncoder(Constants.Drivetrain.frontLeftAbsoluteEncoder, 2.254991, true);
+
+    m_frontLeft = new SwerveWheel(m_frontLeftDrive, m_frontLeftTurn, m_frontLeftEncoder, widthOffset, lengthOffset);
+
+    m_frontRightDrive = new CANSparkMax(Constants.Drivetrain.frontRightDriveMotorPort, MotorType.kBrushless);
+    m_frontRightDrive.setInverted(false);
+    m_frontRightTurn = new CANSparkMax(Constants.Drivetrain.frontRightTurnMotorPort, MotorType.kBrushless);
+    m_frontRightEncoder = new AbsoluteEncoder(Constants.Drivetrain.frontRightAbsoluteEncoder, 2.466641, true);
+
+    m_frontRight = new SwerveWheel(m_frontRightDrive, m_frontRightTurn, m_frontRightEncoder, widthOffset, -lengthOffset);
+
+    m_backLeftDrive = new CANSparkMax(Constants.Drivetrain.rearLeftDriveMotorPort, MotorType.kBrushless);
+    m_backLeftDrive.setInverted(true);
+    m_backLeftTurn = new CANSparkMax(Constants.Drivetrain.rearLeftTurnMotorPort, MotorType.kBrushless);
+    m_backLeftEncoder = new AbsoluteEncoder(Constants.Drivetrain.rearLeftAbsoluteEncoder, .279, true);
+    m_backLeft = new SwerveWheel(m_backLeftDrive, m_backLeftTurn, m_backLeftEncoder, -widthOffset, lengthOffset);
+
+    m_backRightDrive = new CANSparkMax(Constants.Drivetrain.rearRightDriveMotorPort, MotorType.kBrushless);
+    m_backRightDrive.setInverted(false);
+    m_backRightTurn = new CANSparkMax(Constants.Drivetrain.rearRightturnMotorPort, MotorType.kBrushless);
+    m_backRightEncoder = new AbsoluteEncoder(Constants.Drivetrain.rearRightAbsoluteEncoder, 3.925, true);
+    m_backRight = new SwerveWheel(m_backRightDrive, m_backRightTurn, m_backRightEncoder, -widthOffset, -lengthOffset);
   }
 
   public void move(double x, double y, double theta, boolean fieldRelative) {
@@ -70,5 +106,10 @@ public class SwerveDrivetrain extends SubsystemBase {
     } else {
       serveModuleStates = kinematics.toSwerveModuleStates(new ChassisSpeeds(_x, _y, _theta));
     }
+
+    m_frontLeft.setDesiredState(serveModuleStates[0]);
+    m_frontRight.setDesiredState(serveModuleStates[1]);
+    m_backLeft.setDesiredState(serveModuleStates[2]);
+    m_backRight.setDesiredState(serveModuleStates[3]);
   }
 }
--- src/main/java/frc/robot/subsystems/SwerveWheel.java
+++ src/main/java/frc/robot/subsystems/SwerveWheel.java
@@ -1,12 +1,41 @@
 package frc.robot.subsystems;
 
-public class SwerveWheel {
-    private static final double kWheelRadius = 0.0508;
-    private static final int kEncoderResolution = 4096;
-  
-    private static final double kModuleMaxAngularVelocity = Math.PI;
-    private static final double kModuleMaxAngularAcceleration = 2 * Math.PI; // radians per second squared
-    private static final double kTwoPi = 2 * Math.PI;
+import com.revrobotics.CANSparkMax;
 
-    
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+import edu.wpi.first.wpilibj.kinematics.SwerveModuleState;
+import frc.robot.Constants;
+import frc.robot.common.AbsoluteEncoder;
+
+public class SwerveWheel {
+    private CANSparkMax m_drivemotor;
+    private CANSparkMax m_turnmotor;
+    private AbsoluteEncoder m_encoder;
+    private Translation2d m_location;
+    private final PIDController m_turningPID = new PIDController(0.3, 0, 0);
+
+    public SwerveWheel(CANSparkMax drivemotor, CANSparkMax turnmotor, AbsoluteEncoder encoder, double X, double Y) {
+        m_drivemotor = drivemotor;
+        m_turnmotor = turnmotor;
+        m_encoder = encoder;
+        m_location = new Translation2d(X, Y);
+        m_drivemotor.setSmartCurrentLimit(35, 60, 150);
+        m_turnmotor.setSmartCurrentLimit(17, 30, 75);
+
+        m_turningPID.enableContinuousInput(0, 2 * Math.PI);
+    }
+
+    public void setDesiredState(SwerveModuleState target){
+        m_drivemotor.set(target.speedMetersPerSecond/Constants.Swerve.MaxSpeedOfWheel);
+        double pidOutput = m_turningPID.calculate(m_encoder.getRadians(), target.angle.getRadians());
+
+        //capping the pid output at a range between -1 to 1
+        if (pidOutput > 1) {
+            pidOutput = 1;
+        } else if (pidOutput < -1) {
+            pidOutput = -1;
+        }
+        m_turnmotor.set(pidOutput);
+    }
 }
